1f4547d4ea1b5480798f921daccc2c19adb52bc5
Torin Tashima <t.tashima@students.clark.edu>
2019-11-07 14:58:50 -0800

    Demo Nov. 7 Submission

---
+++ Demo1107/ConfigFileTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using static System.Console;
+
+namespace ConfigFileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("*** Fun with config Files ***\n");
+
+            string[] appSettings = new string[5];
+            appSettings[0] = ConfigurationManager.AppSettings["provider1"];
+            appSettings[1] = ConfigurationManager.AppSettings["provider2"];
+            appSettings[2] = ConfigurationManager.AppSettings["connectionString2"];
+            appSettings[3] = ConfigurationManager.AppSettings["provider3"];
+            appSettings[4] = ConfigurationManager.AppSettings["connectionString3"];
+
+            foreach (string s in appSettings)
+            {
+                WriteLine(s);
+            }
+            WriteLine();
+
+            WriteLine(ConfigurationManager.
+                ConnectionStrings["AutoLotOleDbProvider"].ConnectionString);
+        }
+    }
+}
---
+++ Demo1107/FactoryExample/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration; // requires reference update
+using System.Data.Common;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace FactoryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region get config info
+            string dataProvider = ConfigurationManager.AppSettings["provider"];
+            WriteLine("\tprovider: {0}", dataProvider);
+
+            // alternate see p. 833
+            var cnStringBuilder = new SqlConnectionStringBuilder
+            {
+                InitialCatalog = "Northwind",
+                DataSource = @"(localdb)\mssqllocaldb",
+                ConnectTimeout = 30,
+                IntegratedSecurity = true
+            };
+
+            WriteLine($"\tBuilt Connection String: " +
+                $"{cnStringBuilder.ConnectionString}\n");
+            #endregion
+
+            // get factory
+            DbProviderFactory factory =
+                DbProviderFactories.GetFactory(dataProvider);
+
+            using (DbConnection connection = factory.CreateConnection())
+            {
+                if (connection == null)
+                {
+                    WriteLine("There was an issue creating the connection");
+                    return;
+                }
+                else WriteLine("-> Connection created");
+
+                connection.ConnectionString = cnStringBuilder.ConnectionString;
+                connection.Open();
+
+                DbCommand myCommand = factory.CreateCommand();
+                if (myCommand == null)
+                {
+                    WriteLine("There is an issue creating the command");
+                    return;
+                }
+                else WriteLine($"Your command object is a " +
+                    $"{myCommand.GetType().Name}");
+
+                myCommand.Connection = connection;
+                myCommand.CommandText = "Select * from Shippers";
+
+                using (DbDataReader dataReader = myCommand.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        WriteLine($"-> shipper # {dataReader["ShipperId"]} " +
+                            $"name is a {dataReader[1]} phone: {dataReader[2]}");
+                    }
+                }
+            }
+        }
+    }
+}
